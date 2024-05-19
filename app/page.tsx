bd589a14e3c4d3a31c53d57cc7ab0bbba10fe05e
ganaa0918 <ch.gantulga54@gmail.com>
2024-05-19 20:50:10 +0800

    finished

--- app/page.tsx
+++ app/page.tsx
@@ -12,10 +12,10 @@ export default function Home() {
     <div className="flex flex-col lg:flex-row items-center gap-8 p-8 max-w-6xl w-full">
       <div className="flex flex-col space-y-4 lg:w-1/2 w-full">
         <h1 className="text-5xl font-bold">
-        Мэргэжлийн түвшинд хиймэл оюун ухаанар таны зурагыг үүсгэнэ
+        Мэргэжлийн түвшинд хиймэл оюун ухаанаар таны зургийг үүсгэнэ
         </h1>
         <p className="text-gray-600 text-lg">
-        Хаана ч ашиглаж болох зурагаа үүсгэж цаг зав мөнгөө хэмнэ!
+        Хаана ч ашиглаж болох зургаа үүсгэж цаг зав мөнгөө хэмнэ!
         </p>
         <div className="flex flex-col space-y-2">
               <Link href="/faceswap">
@@ -32,7 +32,6 @@ export default function Home() {
       </div>
     </div>
      <ExplainerSection />
-    {/* <PricingSection />  */}
   </div>
   );
 }
